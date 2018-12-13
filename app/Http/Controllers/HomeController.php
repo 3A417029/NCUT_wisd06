e0af165f3ad4a34834d93f9951e18bfc36031838
3A417029 <howardzhu0709@outlook.com>
2018-12-13 17:42:01 +0800

    Create Management Interface

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,35 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\Dashboard;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Column;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('home');
+        return Admin::content(function (Content $content) {
+            $content->header('Dashboard');
+            $content->description('Description...');
+
+            $content->row(Dashboard::title());
+            
+            $content->row(function (Row $row) {
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::environment());
+                });
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::extensions());
+                });
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::dependencies());
+                });
+            });
+        });
     }
 }
